99655a297eb318adcb242ca2513407de88511fb7
borhan98 <borhanuddindns420@gmail.com>
2023-12-01 23:53:39 +0600

    Created get api to get all meals

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middlewares 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hf1udpd.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+
+    const mealCollection = client.db("UniFood").collection("meals");
+
+    /*-----------------------------------------------
+                    Meal Related APIs
+    ------------------------------------------------*/
+    app.get("/meals", async (req, res) => {
+        const result = await mealCollection.find().toArray();
+        res.send(result);
+    })
+
+
+    // Send a ping to confirm a successful connection
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
