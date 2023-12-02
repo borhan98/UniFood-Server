c42581ed07e0ef1de4219023a1db71ae1a7b6f55
borhan98 <borhanuddindns420@gmail.com>
2023-12-03 04:07:22 +0600

    POST API created to store new user

--- index.js
+++ index.js
@@ -27,15 +27,49 @@ async function run() {
     // await client.connect();
 
 
+    const userCollection = client.db("UniFood").collection("users");
     const mealCollection = client.db("UniFood").collection("meals");
+    const reviewCollection = client.db("UniFood").collection("reviews");
+
+    /*-----------------------------------------------
+                    Users Related APIs
+    ------------------------------------------------*/
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const query = { email: newUser.email };
+      const isExist = await userCollection.findOne(query);
+      if (isExist) {
+        return res.send({ message: "User already existing", insertedId: null });
+      }
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    })
 
     /*-----------------------------------------------
                     Meal Related APIs
     ------------------------------------------------*/
     app.get("/meals", async (req, res) => {
-        const result = await mealCollection.find().toArray();
-        res.send(result);
+      const result = await mealCollection.find().toArray();
+      res.send(result);
     })
+
+    app.get("/meals/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: id };
+      const result = await mealCollection.findOne(query);
+      res.send(result);
+    })
+
+    /*-----------------------------------------------
+                Reviews Related APIs
+    ------------------------------------------------*/
+    app.get("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { meal_id: id };
+      const result = await reviewCollection.find(query).toArray();
+      res.send(result)
+    })
+
 
 
     // Send a ping to confirm a successful connection
@@ -49,8 +83,8 @@ run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
-    res.send("UniFood is running");
+  res.send("UniFood is running");
 })
 app.listen(port, () => {
-    console.log(`Unifood is running on port ${port}`);
+  console.log(`Unifood is running on port ${port}`);
 })
